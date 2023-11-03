0c93cc83c72a7b361946331cd31654fb51a44989
EliseCho13 <dbwlspdlqj@naver.com>
2023-11-03 16:46:12 +0900

    feat(fe): added basic footer with member names

--- frontend/src/components/footer.js
+++ frontend/src/components/footer.js
@@ -1,9 +1,35 @@
 import styled from "styled-components";
 
 const Footer = () => {
-  return <div>Github</div>;
+  return (
+    <FooterContainer>
+      Github
+      <NameContainer>
+        <li>김동한</li>
+        <li>안낙균</li>
+        <li>유지훈</li>
+      </NameContainer>
+      <NameContainer>
+        <li>임소리</li>
+        <li>조민호</li>
+        <li>조유지</li>
+      </NameContainer>
+    </FooterContainer>
+  );
 };
 
-styled.div``;
-
+const FooterContainer = styled.div`
+  background: #0fa958;
+  color: white;
+  padding: 2.5rem;
+  display: flex;
+  align-items: center;
+  font-size: 1.25rem;
+`;
+const NameContainer = styled.ul`
+  > li {
+    list-style: none;
+    margin: 0.5rem 0rem;
+  }
+`;
 export default Footer;
