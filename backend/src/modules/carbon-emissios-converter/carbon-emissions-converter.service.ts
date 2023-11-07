2061d930467ab25a9691ee20ca7ff62f361b07ff
Min-Ho CHO <66549638+chominho96@users.noreply.github.com>
2023-11-07 21:21:46 +0900

    feat(be): added carbon emissions converting service

---
+++ backend/src/modules/carbon-emissios-converter/carbon-emissions-converter.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@nestjs/common';
+import { CarbonEmissionsConvertedValuesDto } from './carbon-emissions-converted-values.dto';
+
+/**
+ * @file carbon-emissions-converter.service.ts
+ * @description 탄소 배출량을 일상생활에서의 각종 사용량으로 변환하는 역할을 담당하는 클래스입니다.
+ * @author Min Ho CHO
+ * TODO: 제공하는 dto 타입을 값으로 제공할 것인지, string 타입으로 제공할 것인지 협의 필요
+ */
+@Injectable()
+export class CarbonEmissionsConverterService {
+  readonly SOUTH_KOREA_CI = 180;
+  readonly GRAMS_TO_KILOGRAMS = 1000;
+  readonly EMISSIONS_OF_AVERAGE_PASSENGER_IN_EUROPE = 175;
+  readonly PARIS_TO_LONDON_CARBON_EMISSIONS = 50000;
+
+  /**
+   * 탅소 배출량을 입력으로 받아, 실생활 사용량으로 변환하는 메서드입니다.
+   * @param carbonEmission 탄소 배출량 (gCo2e)
+   */
+  getConvertedCarbonEmissionsValues(
+    carbonEmission: number,
+  ): CarbonEmissionsConvertedValuesDto {
+    // 1. Energy 계산
+    // kWh 단위
+    const energy: number = carbonEmission / this.SOUTH_KOREA_CI;
+
+    // 2. Tree-months 계산
+    const treeMonths: number =
+      (carbonEmission / this.GRAMS_TO_KILOGRAMS / 11) * 12;
+
+    // 3. passage car 계산
+    // passage car은 유럽을 기준으로 계산
+    // km 단위
+    const passageCar: number =
+      carbonEmission / this.EMISSIONS_OF_AVERAGE_PASSENGER_IN_EUROPE;
+
+    // 4. flight 계산
+    // flight는 paris to london을 기준으로 계산
+    // % 단위
+    const flight: number =
+      (carbonEmission / this.PARIS_TO_LONDON_CARBON_EMISSIONS) * 100;
+
+    return new CarbonEmissionsConvertedValuesDto(
+      carbonEmission,
+      energy,
+      treeMonths,
+      passageCar,
+      flight,
+    );
+  }
+}
