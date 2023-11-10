60d880921dd13d2865d17d7dfab44f8128a358a6
EliseCho13 <dbwlspdlqj@naver.com>
2023-11-10 15:25:26 +0900

    fix(fe): fixed merge error

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,25 +5,27 @@ import Footer from "./components/footer";
 import HowItWorks from "./components/howItWorks";
 import styled from "styled-components";
 
-
 function App() {
   return (
-    <Wrapper>
+    <>
       <Header />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <HowItWorks />
+      <Wrapper>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+        <HowItWorks />
+      </Wrapper>
+
       <Footer />
-    </Wrapper>
+    </>
   );
 }
 
