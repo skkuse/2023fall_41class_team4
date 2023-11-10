47583af1e15caba56014f13da33ca745f0087b82
EliseCho <dbwlspdlqj@naver.com>
2023-11-10 09:52:08 +0900

    feat: 설명 컴포넌트 추가 (#12)
    * setting: edited issue template asignees
    
    * feat: set up nestjs
    
    * chore: use pnpm for both projects
    
    * chore: add MIT LICENSE (#6)
    
    * feat(fe): added basic info and frame
    
    * style(fe): changed css
    
    * feat(fe): added mathjax, leaf icon
    
    ---------
    
    Co-authored-by: Min-Ho CHO <66549638+chominho96@users.noreply.github.com>
    Co-authored-by: Sori Lim <aint.imsorry@gmail.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,16 +2,16 @@ import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/header";
 import Footer from "./components/footer";
+import HowItWorks from "./components/howItWorks";
+import styled from "styled-components";
+
 
 function App() {
   return (
-    <>
+    <Wrapper>
       <Header />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -21,9 +21,17 @@ function App() {
           Learn React
         </a>
       </header>
+      <HowItWorks />
       <Footer />
-    </>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
 
 export default App;
