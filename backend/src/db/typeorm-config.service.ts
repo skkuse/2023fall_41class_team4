04bf7bd3e81933ae92ee56d363cfc6b56635fbaa
Sori Lim <aint.imsorry@gmail.com>
2023-11-17 07:25:04 +0000

    feat: use typeorm

---
+++ backend/src/db/typeorm-config.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+@Injectable()
+export class TypeOrmConfigService {
+  constructor(private readonly config: ConfigService) {}
+
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+    const options: TypeOrmModuleOptions = {
+      type: 'mysql',
+      host: this.config.get('DATABASE_HOST'),
+      port: this.config.get<number>('DATABASE_PORT'),
+      username: this.config.get('DATABASE_USER'),
+      password: this.config.get('DATABASE_PASS'),
+      database: this.config.get('DATABASE_NAME'),
+      entities: [],
+      synchronize: true,
+    }
+    
+    return options
+  }
+}
---
+++ typeorm.config.js
@@ -0,0 +1,11 @@
+{
+    type: "mysql",
+    host: "localhost",
+    port: 3306,
+    username: "test",
+    password: "test",
+    database: "test",
+    entities: [/*...*/],
+    migrations: [/*...*/],
+    migrationsTableName: "custom_migration_table",
+}
