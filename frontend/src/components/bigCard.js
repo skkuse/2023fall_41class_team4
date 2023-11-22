15c37ed7b8902ff0e8f0660e32828994bdaf292d
EliseCho13 <dbwlspdlqj@naver.com>
2023-11-22 20:22:40 +0900

    style(fe): changed font, item align of bigcard

--- frontend/src/components/bigCard.js
+++ frontend/src/components/bigCard.js
@@ -20,27 +20,32 @@ const CardContainer = styled.div`
   padding: 3rem;
   background: #f8f8f8;
   border-radius: 1.5rem;
-
   box-shadow: inset 0.2rem 0.6rem 0.2rem 0.2rem rgba(0, 0, 0, 0.2);
-  > div:last-child {
-    display: flex;
-    align-items: center;
-    justify-content: space-around;
-  }
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
   > div:first-child {
-    font-size: 1.2rem;
+    font-size: 1.5rem;
     .icon {
       color: #0fa958;
       margin-right: 0.6rem;
     }
   }
+  > div:last-child {
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+  }
 `;
 const ValueBox = styled.div`
   font-size: 3rem;
   font-weight: bold;
+  margin-left: 1.8rem;
 `;
 const UnitBox = styled.div`
   color: darkgray;
   font-size: 1rem;
+  margin-left: 0.4rem;
 `;
 export default BigCard;
