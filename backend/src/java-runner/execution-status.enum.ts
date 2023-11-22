e8b4af1376311205c7587e35e1776ef515a0c4a1
kyun <annakky@naver.com>
2023-11-23 01:42:27 +0900

    feat: java runner 구현

---
+++ backend/src/java-runner/execution-status.enum.ts
@@ -0,0 +1,4 @@
+export enum ExecutionStatus {
+  SUCCESS = 'SUCCESS',
+  FAIL = 'FAIL',
+}
---
+++ backend/src/java-runner/java-runner.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { ExecutionResult } from './execution-result.domain';
+import { execSync } from 'child_process';
+import { readFileSync } from 'fs';
+
+@Injectable()
+export class JavaRunnerService {
+  readonly tempDirectory = './temp';
+  readonly fileName = 'Main';
+  readonly resultFileName = 'test.txt';
+
+  async run(): Promise<ExecutionResult> {
+    const command = `/usr/bin/time -f \"%e %P %M\" -o ${this.resultFileName} java -cp ${this.tempDirectory} ${this.fileName}`;
+    execSync(command);
+
+    const resultFile = readFileSync(`${this.resultFileName}`, {
+      encoding: 'utf-8',
+    });
+
+    const [runtime, cpuUsage, memoryUsage] = resultFile
+      .toString()
+      .trim()
+      .split(' ');
+
+    return new ExecutionResult(runtime, cpuUsage.slice(0, -1), memoryUsage);
+  }
+}
