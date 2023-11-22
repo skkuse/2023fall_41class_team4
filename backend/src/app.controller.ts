a0742a2daae046f952895b562ce3d59aeed355e4
kyun <annakky@naver.com>
2023-11-23 02:11:10 +0900

    feat: 탄소 배출 api interface 개발

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { CarbonEmissionDto } from './carbon-emission.dto';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post('carbon-emissions')
+  async calculateEmissions(@Body() body: any): Promise<CarbonEmissionDto> {
+    await this.appService.calculateEmission(body.code);
+
+    return new CarbonEmissionDto(1000, 30000, 120, 0.2, 15, 26000);
   }
 }
--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { JavaCompilerService } from './java-compiler/java-compiler.service';
+import { JavaRunnerService } from './java-runner/java-runner.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, JavaCompilerService, JavaRunnerService],
 })
 export class AppModule {}
