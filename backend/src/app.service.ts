6260cec7905e9e077aca9775d70417e666d49f32
kyun <annakky@naver.com>
2023-11-23 23:18:43 +0900

    feat: 탄소배출량 계산 api mock 데이터 추가 및 swagger 추가 (#31)

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { JavaCompilerService } from './java-compiler/java-compiler.service';
+import { JavaRunnerService } from './java-runner/java-runner.service';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    private readonly javaRunnerService: JavaRunnerService,
+    private readonly javaCompilerService: JavaCompilerService,
+  ) {}
+
+  async calculateEmission(code: string) {
+    await this.javaCompilerService.compile(code);
+
+    const executionResult = await this.javaRunnerService.run();
+
+    // TODO carbon calculator 호출
   }
 }
