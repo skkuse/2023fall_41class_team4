f78cbfc23f68377688d37adafe0ae6b24292b4df
jihooni0914 <dbwlgns8314@gmail.com>
2023-11-29 19:10:17 +0900

    fix: apply file path argument

--- process/run.sh
+++ process/run.sh
@@ -2,7 +2,7 @@
 
 
 # input file path를 argument로 넘겨 받고 file path 변수에 저장하기
-input_path="./input.java"
+input_path="./Input.java"
 if [ "$#" -eq 1 ]; then
     input_path=$1
 fi
@@ -17,12 +17,11 @@ if [ $? -ne 0 ]; then
 fi
 
 # 실행 및 결과 분석
-opt=$(basename "$file")
+opt=$(basename "${input_path%.java}")
 run_result=$(./run $opt)
 IFS=' ' read -r status_code result_type result_message <<< "$run_result"
     
 echo $status_code $result_type $result_message > $result
-
 # status code에 따른 분기
 # if [ "$status_code" -eq 1 ]; then
 #     # status code가 1인 경우
