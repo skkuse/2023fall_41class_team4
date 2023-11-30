4d97baf8feac41c554b49878264dc37c1d35ce05
Donghan Kim <54224868+pendant-k@users.noreply.github.com>
2023-11-30 16:27:53 +0900

    feat(fe): add refresh btn

--- frontend/src/components/JavaEditor.js
+++ frontend/src/components/JavaEditor.js
@@ -1,6 +1,8 @@
 import React, { useRef } from "react";
 import Editor from "@monaco-editor/react";
 import styled from "styled-components";
+import { faRefresh } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const JavaEditor = () => {
     const editorRef = useRef(null);
@@ -34,6 +36,9 @@ const JavaEditor = () => {
             </EditorContainer>
             <BtnContainer>
                 <SubmitBtn onClick={handleClick}>Submit</SubmitBtn>
+                <RefreshBtn onClick={() => editorRef.current.setValue("")}>
+                    <FontAwesomeIcon icon={faRefresh} className="icon" />
+                </RefreshBtn>
             </BtnContainer>
         </>
     );
@@ -63,6 +68,23 @@ const SubmitBtn = styled.button`
     background: #38c972;
     box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
     color: #f8f8f8;
+    font-family: Inter;
+    text-align: center;
+    font-size: 20px;
+    font-style: normal;
+    font-weight: 800;
+    line-height: normal;
+`;
+
+const RefreshBtn = styled.button`
+    width: 5rem;
+    height: 3rem;
+    margin-bottom: 4rem;
+    margin-left: 1rem;
+    border-radius: 15px;
+    border: none;
+    background: #ffffff;
+    box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
     font-family: Inter;
     text-align: center;
     font-size: 20px;
