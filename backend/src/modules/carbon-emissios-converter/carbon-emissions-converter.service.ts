27f1b24cc415a623495ce384644bf100a5a16ca3
HOYA <66549638+chominho96@users.noreply.github.com>
2023-12-03 16:32:25 +0900

    feat: 탄소 배출량을 실생활 사용량으로 바꾸는 로직 개발 (#26)
    * feat(be): added carbon emissions converted values dto
    
    * feat(be): added CarbonEmissionsConvertedValuesDto's constructor
    
    * feat(be): added carbon emissions converting service
    
    * refactor(be): deleted not null condition to dto
    
    * chore(be): edited service comment
    
    * chore(be): resolved todo
    
    * fix(be): fix output format
    
    * fix(be): edited real life usage list and reformat dto
    
    * refactor(be): refactor dto constructor parameter
    
    * fix(be): edited calculation logic of real usage

---
+++ backend/src/modules/carbon-emissios-converter/carbon-emissions-converter.service.ts
@@ -0,0 +1,59 @@
+import { Injectable } from '@nestjs/common';
+import { CarbonEmissionsConvertedValuesDto } from './carbon-emissions-converted-values.dto';
+
+/**
+ * @file carbon-emissions-converter.service.ts
+ * @description 탄소 배출량을 일상생활에서의 각종 사용량으로 변환하는 역할을 담당하는 클래스입니다.
+ * @author Min Ho CHO
+ */
+@Injectable()
+export class CarbonEmissionsConverterService {
+  readonly SOUTH_KOREA_CI = 180;
+  readonly TV_CARBON_EMISSION_PER_HOUR = 88;
+  readonly CARBON_EMISSIONS_OF_AVERAGE_PASSENGER_IN_EUROPE = 175;
+  readonly KILO_WATT_TO_WATT = 1000;
+  readonly CARBON_EMISSIONS_OF_ELEVATOR_PER_FLOOR = 2;
+  readonly CARBON_EMISSIONS_OF_A4_PER_SHEET = 4.5;
+
+  /**
+   * 탄소 배출량을 입력으로 받아, 실생활 사용량으로 변환하는 메서드입니다.
+   * @param carbonEmission 탄소 배출량 (gCo2e 단위)
+   */
+  getConvertedCarbonEmissionsValues(
+    carbonEmission: number,
+  ): CarbonEmissionsConvertedValuesDto {
+    // 1. 전력 소모량 계산
+    // kWh 단위
+    const energy: number = carbonEmission / this.SOUTH_KOREA_CI;
+
+    // 2. TV 시청 시간 계산
+    // 시간 단위
+    const tvWatchingTime: number =
+      carbonEmission / this.TV_CARBON_EMISSION_PER_HOUR;
+
+    // 3. 승용차 주행 거리 계산
+    // km 단위
+    const passageCar: number =
+      carbonEmission / this.CARBON_EMISSIONS_OF_AVERAGE_PASSENGER_IN_EUROPE;
+
+    // 4. 엘레베이터 층수 이동 계산
+    // ~개 단위
+    const elevatorFloorMovement: number =
+      (energy * this.KILO_WATT_TO_WATT) /
+      this.CARBON_EMISSIONS_OF_ELEVATOR_PER_FLOOR;
+
+    // 5. A4 용지 개수 계산
+    // ~장 단위
+    const a4PaperUsage: number =
+      carbonEmission / this.CARBON_EMISSIONS_OF_A4_PER_SHEET;
+
+    return new CarbonEmissionsConvertedValuesDto(
+      carbonEmission,
+      energy,
+      tvWatchingTime,
+      passageCar,
+      elevatorFloorMovement,
+      a4PaperUsage,
+    );
+  }
+}
