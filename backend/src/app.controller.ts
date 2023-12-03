4302df72e507334ce375e4bc36796fc14571f454
Sori Lim <aint.imsorry@gmail.com>
2023-12-03 07:15:27 +0000

    feat: throw error if code size is bigger than 4KB

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,4 +1,10 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  InternalServerErrorException,
+  Post,
+  UnprocessableEntityException,
+} from '@nestjs/common';
 import { AppService } from './app.service';
 import { CarbonEmissionResponseDto } from './carbon-emission-response.dto';
 import { ApiCreatedResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
@@ -15,7 +21,15 @@ export class AppController {
   async calculateEmissions(
     @Body() body: CarbonEmissionRequestDto,
   ): Promise<CarbonEmissionResponseDto> {
-    await this.appService.calculateEmission(body.code);
+    try {
+      await this.appService.calculateEmission(body.code);
+    } catch (error) {
+      if (error instanceof UnprocessableEntityException) {
+        throw new UnprocessableEntityException(error.message);
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
 
     return new CarbonEmissionResponseDto(1000, 30000, 120, 0.2, 15, 26000);
   }
--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,19 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnprocessableEntityException } from '@nestjs/common';
 import { JavaCompilerService } from './java-compiler/java-compiler.service';
 import { JavaRunnerService } from './java-runner/java-runner.service';
+import { CodeService } from './code/code.service';
 
 @Injectable()
 export class AppService {
   constructor(
-    private readonly javaRunnerService: JavaRunnerService,
+    private readonly codeService: CodeService,
     private readonly javaCompilerService: JavaCompilerService,
+    private readonly javaRunnerService: JavaRunnerService,
   ) {}
 
-  async calculateEmission(code: string) {
+  async calculateEmission(input: string) {
+    const size = new Blob([input]).size;
+    if (size > 4000) {
+      throw new UnprocessableEntityException(
+        '입력하는 코드의 사이즈는 4KB 이하여야 합니다.',
+      );
+    }
+
+    const code = await this.codeService.saveCode(input);
+    // FIXME: compile result 넘겨주도록 수정
     await this.javaCompilerService.compile(code);
+    const executionResult = await this.javaRunnerService.run(code);
 
-    const executionResult = await this.javaRunnerService.run();
+    const emission = await this.codeService.calculateEmission(executionResult);
+    await this.codeService.updateEmission({ id: code.id, emission: emission });
 
-    // TODO carbon calculator 호출
+    // TODO: 실생활 변환 후 Response로 가공해서 return
   }
 }
