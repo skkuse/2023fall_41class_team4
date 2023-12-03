1a45a5adb6ce2e3e21bd2112f5cd1a193e6d6317
EliseCho13 <dbwlspdlqj@naver.com>
2023-12-03 15:37:19 +0900

    added max limit of editor, changed mouse curor

--- frontend/src/components/JavaEditor.js
+++ frontend/src/components/JavaEditor.js
@@ -6,149 +6,162 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 // status 텍스트 변경을 위한 상수
 const Status = {
-    WAITING: "WAITING",
-    PROGRESS: "PROGRESS",
-    SUCCESS: "SUCCESS",
-    BUILDERROR: "BUILDERROR",
-    COMPILEERROR: "COMPILEERROR",
+  WAITING: "WAITING",
+  PROGRESS: "PROGRESS",
+  SUCCESS: "SUCCESS",
+  BUILDERROR: "BUILDERROR",
+  COMPILEERROR: "COMPILEERROR",
 };
 
 const JavaEditor = () => {
-    const defaultValue = "// write down your code here";
-    const editorRef = useRef(null);
+  const defaultValue = "// write down your code here";
+  const editorRef = useRef(null);
 
-    const [status, setStatus] = useState(Status.WAITING);
+  const [status, setStatus] = useState(Status.WAITING);
+  const [lineCount, setLineCount] = useState();
 
-    function getStatusText(status) {
-        switch (status) {
-            case Status.WAITING:
-                return "코드를 입력해주세요.";
+  function getStatusText(status) {
+    switch (status) {
+      case Status.WAITING:
+        return "코드를 입력해주세요.";
 
-            case Status.PROGRESS:
-                return "코드를 실행중입니다.";
+      case Status.PROGRESS:
+        return "코드를 실행중입니다.";
 
-            case Status.SUCCESS:
-                return "코드가 성공적으로 컴파일 되었습니다!";
+      case Status.SUCCESS:
+        return "코드가 성공적으로 컴파일 되었습니다!";
 
-            default:
-                return "코드를 입력해주세요.";
-        }
+      default:
+        return "코드를 입력해주세요.";
     }
+  }
 
-    function handleEditorDidMount(editor, monaco) {
-        // set editorRef.current to editor
-        editorRef.current = editor;
-    }
+  function handleEditorDidMount(editor, monaco) {
+    // set editorRef.current to editor
+    setLineCount(editor.getModel().getLineCount());
+    editorRef.current = editor;
+  }
 
-    function handleClick() {
-        // send code to backend
-        setStatus(Status.PROGRESS);
-        setTimeout(() => {
-            if (true) {
-                setStatus(Status.SUCCESS);
-            }
-        }, 2000);
+  function handleClick() {
+    // send code to backend
+    setStatus(Status.PROGRESS);
+    setTimeout(() => {
+      if (true) {
+        setStatus(Status.SUCCESS);
+      }
+    }, 2000);
+    // console.log(editorRef.current.getValue());
+    if (lineCount > 1000) {
+      alert("1000줄 이내로 작성해주시길 바랍니다.");
+    } else {
+      //axios
     }
-    function handleRefresh() {
-        editorRef.current.setValue(defaultValue);
-    }
+  }
+  function handleRefresh() {
+    editorRef.current.setValue(defaultValue);
+  }
 
-    return (
-        <>
-            <EditorContainer>
-                <Editor
-                    width="60vw"
-                    height="50vh"
-                    defaultLanguage="java"
-                    defaultValue={defaultValue}
-                    options={{
-                        theme: "vs-light",
-                        minimap: {
-                            enabled: false,
-                        },
-                        fontSize: 14,
-                        readOnly: status === Status.PROGRESS ? true : false,
-                        readOnlyMessage: { value: "코드를 실행중입니다." },
-                    }}
-                    onMount={handleEditorDidMount}
-                />
-            </EditorContainer>
-            <BtnContainer>
-                <SubmitBtn
-                    onClick={handleClick}
-                    $onProgress={status === Status.PROGRESS}
-                >
-                    Submit
-                </SubmitBtn>
-                <RefreshBtn onClick={handleRefresh}>
-                    <FontAwesomeIcon icon={faRefresh} className="icon" />
-                </RefreshBtn>
-                <StatusText $status={status}>
-                    {getStatusText(status)}
-                </StatusText>
-            </BtnContainer>
-        </>
-    );
+  return (
+    <>
+      <EditorContainer>
+        <Editor
+          width="60vw"
+          height="50vh"
+          defaultLanguage="java"
+          defaultValue={defaultValue}
+          options={{
+            theme: "vs-light",
+            minimap: {
+              enabled: false,
+            },
+            fontSize: 14,
+            readOnly: status === Status.PROGRESS ? true : false,
+            readOnlyMessage: { value: "코드를 실행중입니다." },
+            wordWrap: true,
+          }}
+          onChange={() => {
+            setLineCount(editorRef.current.getModel().getLineCount());
+          }}
+          onMount={handleEditorDidMount}
+        />
+      </EditorContainer>
+      <BtnContainer>
+        <SubmitBtn
+          onClick={handleClick}
+          $onProgress={status === Status.PROGRESS}
+        >
+          Submit
+        </SubmitBtn>
+        <RefreshBtn onClick={handleRefresh}>
+          <FontAwesomeIcon icon={faRefresh} className="icon" />
+        </RefreshBtn>
+        <StatusText $status={status}>{getStatusText(status)}</StatusText>
+      </BtnContainer>
+    </>
+  );
 };
 
 const EditorContainer = styled.div`
-    width: 67vw;
-    background: white;
-    margin: 4rem auto 1rem auto;
-    padding: 1rem;
-    box-shadow: 1px 3px 3px 3px rgba(0, 0, 0, 0.1);
-    border-radius: 15px;
+  width: 67vw;
+  background: white;
+  margin: 4rem auto 1rem auto;
+  padding: 1rem;
+  box-shadow: 1px 3px 3px 3px rgba(0, 0, 0, 0.1);
+  border-radius: 15px;
 `;
 
 const BtnContainer = styled.div`
-    width: 70vw;
-    display: flex;
-    justify-content: flex-start;
+  width: 70vw;
+  display: flex;
+  justify-content: flex-start;
 `;
 
 const SubmitBtn = styled.button`
-    width: 10rem;
-    height: 3rem;
-    margin-bottom: 4rem;
-    border-radius: 15px;
-    border: none;
-    background: ${(props) => (props.$onProgress ? "gray" : "#38C972")};
-    box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
-    color: #f8f8f8;
-    font-family: Inter;
-    text-align: center;
-    font-size: 20px;
-    font-style: normal;
-    font-weight: 800;
-    line-height: normal;
+  width: 10rem;
+  height: 3rem;
+  margin-bottom: 4rem;
+  border-radius: 15px;
+  border: none;
+  background: ${(props) => (props.$onProgress ? "gray" : "#38C972")};
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+  color: #f8f8f8;
+  font-family: Inter;
+  text-align: center;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 800;
+  line-height: normal;
+  &:hover {
+    cursor: ${(props) => (props.$onProgress ? "" : "pointer")};
+  }
 `;
 
 const RefreshBtn = styled.button`
-    width: 5rem;
-    height: 3rem;
-    margin-bottom: 4rem;
-    margin-left: 1rem;
-    margin-right: 1rem;
-    border-radius: 15px;
-    border: none;
-    background: #ffffff;
-    box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
-    font-family: Inter;
-    text-align: center;
-    font-size: 20px;
-    font-style: normal;
-    font-weight: 800;
-    line-height: normal;
+  width: 5rem;
+  height: 3rem;
+  margin-bottom: 4rem;
+  margin-left: 1rem;
+  margin-right: 1rem;
+  border-radius: 15px;
+  border: none;
+  background: #ffffff;
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+  font-family: Inter;
+  text-align: center;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 800;
+  line-height: normal;
 `;
 
 const StatusText = styled.p`
-    font-size: 1rem;
-    font-weight: bold;
-    font-family: Inter;
-    color: ${(props) =>
-        props.$status === Status.BUILDERROR || Status.COMPILEERROR
-            ? "#38C972"
-            : "#C94138"};
+  font-size: 1rem;
+  font-weight: bold;
+  font-family: Inter;
+  color: ${(props) =>
+    props.$status === Status.BUILDERROR || Status.COMPILEERROR
+      ? "#38C972"
+      : "#C94138"};
 `;
 
 export default JavaEditor;
