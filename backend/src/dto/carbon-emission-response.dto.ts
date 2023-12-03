7f048b5693d17b741a4b35642ed37f9b037429fb
Min-Ho CHO <66549638+chominho96@users.noreply.github.com>
2023-12-03 23:49:23 +0900

    refactor(be): refactor carbon emission response

--- backend/src/dto/carbon-emission-response.dto.ts
+++ backend/src/dto/carbon-emission-response.dto.ts
@@ -4,33 +4,26 @@ import { CarbonEmissionConvertedResultResponseDtoDto } from './carbon-emission-c
 
 export class CarbonEmissionResponseDto {
   @ApiProperty({
-    description: '실행 결과',
+    description: '코드 실행 결과',
     example:
       'OK (성공), COMPILE_ERROR (컴파일 에러), RUNTIME_ERROR (런타임 에러)',
   })
   readonly status: string;
 
   @ApiProperty({
-    description: '탄소 배출량 (실행 실패 시 null)',
-    example: 1000,
-  })
-  readonly carbonFootPrint: number;
-
-  @ApiProperty({
-    description: '실생활 사용량 (실행 실패 시 null)',
+    description: '탄소 배출량 및 실생활 사용량 (실행 실패 시 null)',
     example: new CarbonEmissionConvertedResultResponseDtoDto(
       new CarbonEmissionConvertedResultDto(1000, 30000, 120, 0.2, 15, 26000),
     ),
   })
-  readonly convertedResult: CarbonEmissionConvertedResultResponseDtoDto;
+  readonly result: CarbonEmissionConvertedResultResponseDtoDto;
 
   constructor(
     status: string,
     convertedResult: CarbonEmissionConvertedResultDto,
   ) {
     this.status = status;
-    this.carbonFootPrint = convertedResult.carbonFootPrint;
-    this.convertedResult = new CarbonEmissionConvertedResultResponseDtoDto(
+    this.result = new CarbonEmissionConvertedResultResponseDtoDto(
       convertedResult,
     );
   }
