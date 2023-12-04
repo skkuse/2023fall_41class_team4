074bdddb879b05ec897a36b33dfe8f8d6906e3b3
jihooni0914 <dbwlgns8314@gmail.com>
2023-12-05 04:41:33 +0900

    fix: apply process compiler & runner

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -7,28 +7,36 @@ import { CarbonEmissionResponseDto } from './dto/carbon-emission-response.dto';
 
 @Injectable()
 export class AppService {
-  readonly OK_STATUS = 'OK';
-  readonly COMPILE_ERROR_STATUS = 'COMPILE_ERROR';
-  readonly RUNTIME_ERROR_STATUS = 'RUNTIME_ERROR';
-  constructor(
-    private readonly codeService: CodeService,
-    private readonly javaCompilerService: JavaCompilerService,
-    private readonly javaRunnerService: JavaRunnerService,
-    private readonly converterService: ConverterService,
-  ) {}
+    readonly OK_STATUS = 'OK';
+    readonly COMPILE_ERROR_STATUS = 'COMPILE_ERROR';
+    readonly RUNTIME_ERROR_STATUS = 'RUNTIME_ERROR';
+    constructor(
+        private readonly codeService: CodeService,
+        private readonly javaCompilerService: JavaCompilerService,
+        private readonly javaRunnerService: JavaRunnerService,
+        private readonly converterService: ConverterService,
+    ) { }
 
-  async calculateEmission(input: string) {
-    const code = await this.codeService.saveCode(input);
-    // FIXME: compile result 넘겨주도록 수정
-    await this.javaCompilerService.compile(code);
-    const executionResult = await this.javaRunnerService.run(code);
+    async calculateEmission(input: string) {
+        const code = await this.codeService.saveCode(input);
 
-    const emission = await this.codeService.calculateEmission(executionResult);
-    await this.codeService.updateEmission({ id: code.id, emission: emission });
+        // FIXME: compile result 넘겨주도록 수정
+        // "0" : compile error, "1" : compile success
+        const compileResult = await this.javaCompilerService.compile(code);
 
-    return new CarbonEmissionResponseDto(
-      this.OK_STATUS,
-      this.converterService.convertCarbonEmission(emission),
-    );
-  }
+        // FIXME: compile error라면 src/process/result.txt 읽고 넘겨주기
+        if (compileResult === '0') {
+
+        }
+
+        const executionResult = await this.javaRunnerService.run(code);
+
+        const emission = await this.codeService.calculateEmission(executionResult);
+        await this.codeService.updateEmission({ id: code.id, emission: emission });
+
+        return new CarbonEmissionResponseDto(
+            this.OK_STATUS,
+            this.converterService.convertCarbonEmission(emission),
+        );
+    }
 }
