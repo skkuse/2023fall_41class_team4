b559794df40041080619a3d95a829ce029642f1e
Sori Lim <aint.imsorry@gmail.com>
2023-12-05 19:12:29 +0000

    feat: update code after compile error

--- backend/src/java-compiler/java-compiler.module.ts
+++ backend/src/java-compiler/java-compiler.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { JavaCompilerService } from './java-compiler.service';
+import { CodeModule } from 'src/code/code.module';
 
 @Module({
+  imports: [CodeModule],
   providers: [JavaCompilerService],
   exports: [JavaCompilerService],
 })
--- backend/src/java-compiler/java-compiler.service.ts
+++ backend/src/java-compiler/java-compiler.service.ts
@@ -4,11 +4,15 @@ import { existsSync } from 'fs';
 import { Code } from 'src/db/code.entity';
 import { CompileError } from 'src/common/java-error.exception';
 import { mkdir, writeFile } from 'fs/promises';
+import { CodeService } from 'src/code/code.service';
+import { ExecutionStatus } from 'src/db/execution-status.enum';
 
 @Injectable()
 export class JavaCompilerService {
-  readonly baseDir = '/tmp/sandbox';
-  readonly filename = 'Main.java';
+  private readonly baseDir = '/tmp/sandbox';
+  private readonly filename = 'Main.java';
+
+  constructor(private readonly codeService: CodeService) {}
 
   async compile(code: Code) {
     const path = `${this.baseDir}/${code.id}`;
@@ -17,6 +21,16 @@ export class JavaCompilerService {
     try {
       execSync(`javac -d ${path} ${path}/${this.filename}`);
     } catch (error) {
+      await this.codeService.saveExecutionResult(
+        code.id,
+        {
+          status: ExecutionStatus.COMPILE_ERROR,
+          runtime: -1,
+          coreUsage: -1,
+          memUsage: -1,
+        },
+        true, // code 업데이트
+      );
       throw new CompileError(error.stderr.toString());
     }
   }
