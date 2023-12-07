9ee95344eb517043fb48a2728a21646ed28b836e
Donghan Kim <54224868+pendant-k@users.noreply.github.com>
2023-12-07 18:06:56 +0900

    feat: API 연결 (#73)
    * feat(fe): add axios package
    
    * feat(fe): separate status data from editor component
    
    * feat(fe): lift states up from editor to App component
    
    * feat(fe): add api request on app component
    
    * feat(fe): add response params to card component
    
    * chore(fe): remove unused constant file
    
    * feat(fe): remove dummy data

--- frontend/src/components/cardContainer.js
+++ frontend/src/components/cardContainer.js
@@ -8,73 +8,68 @@ import { faCarSide } from "@fortawesome/free-solid-svg-icons";
 import { faTrainSubway } from "@fortawesome/free-solid-svg-icons";
 import { faAppleWhole } from "@fortawesome/free-solid-svg-icons";
 
-const response = {
-  //dummy data
-  runtime: "0.3h",
-  coreCount: "2",
-  cpuWatt: "15W",
-  cpuPercent: "80%",
-  memory: "3GB",
-  wattPerGB: "0.3725W/GB",
-  efficiency: "1.125",
-  co2: "14.04kg",
-  power: "14.04kWh",
-  tv: "3h",
-  car: "10km",
-  subway: "20m",
-  apple: "30g",
-};
-
-const CardComponent = () => {
-  return (
-    <BoxWrapper>
-      <BigContainer>
-        <BigCard icon={faEnvira} name="탄소 배출량" value={response.co2} />
-        <BigCard icon={faBolt} name="전력 소모량" value={response.power} />
-      </BigContainer>
-      <SmallContainer>
-        <SmallCard icon={faTv} name="TV 시청 시간" value={response.tv} />
-        <SmallCard
-          icon={faCarSide}
-          name="승용차 주행 거리"
-          value={response.car}
-        />
-        <SmallCard
-          icon={faTrainSubway}
-          name="지하철 이동 거리"
-          value={response.subway}
-        />
-        <SmallCard
-          icon={faAppleWhole}
-          name="사과 생산량"
-          value={response.apple}
-        />
-      </SmallContainer>
-    </BoxWrapper>
-  );
+const CardComponent = ({ response }) => {
+    return (
+        <BoxWrapper>
+            <BigContainer>
+                <BigCard
+                    icon={faEnvira}
+                    name="탄소 배출량"
+                    value={response.carbonFootPrint}
+                />
+                <BigCard
+                    icon={faBolt}
+                    name="전력 소모량"
+                    value={response.energy}
+                />
+            </BigContainer>
+            <SmallContainer>
+                <SmallCard
+                    icon={faTv}
+                    name="TV 시청 시간"
+                    value={response.tvWatchingTime}
+                />
+                <SmallCard
+                    icon={faCarSide}
+                    name="승용차 주행 거리"
+                    value={response.passengerCarMileage}
+                />
+                <SmallCard
+                    icon={faTrainSubway}
+                    name="지하철 이동 거리"
+                    value={response.subwayTravelDistance}
+                />
+                <SmallCard
+                    icon={faAppleWhole}
+                    name="사과 생산량"
+                    value={response.appleProduction}
+                />
+            </SmallContainer>
+        </BoxWrapper>
+    );
 };
 
 const BoxWrapper = styled.div`
-  margin: 2rem;
-  background: white;
-  border-radius: 2rem;
-  width: 70vw;
-  border-radius: 2rem;
-  box-shadow: 1px 5px 5px 5px rgba(0, 0, 0, 0.2);
+    margin: 2rem;
+    background: white;
+    border-radius: 2rem;
+    width: 70vw;
+    border-radius: 2rem;
+    box-shadow: 1px 5px 5px 5px rgba(0, 0, 0, 0.2);
 `;
 
 const BigContainer = styled.div`
-  padding: 1rem;
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
+    padding: 1rem;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
 `;
 
 const SmallContainer = styled.div`
-  padding: 1rem;
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
+    padding: 1rem;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
 `;
 
 export default CardComponent;
