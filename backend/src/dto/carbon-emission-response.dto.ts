cb3e983bd6682fd4223353cac56ed3a953939a31
Min-Ho CHO <66549638+chominho96@users.noreply.github.com>
2023-12-07 20:43:18 +0900

    fix: fix tv watching unit

--- backend/src/dto/carbon-emission-response.dto.ts
+++ backend/src/dto/carbon-emission-response.dto.ts
@@ -48,7 +48,7 @@ export class CarbonEmissionResponseDto {
     this.memoryUsage = memoryUsage.toFixed(2) + 'MB';
     this.carbonFootPrint = dto.carbonFootPrint.toFixed(2) + 'µgCO2e';
     this.energy = dto.energy.toFixed(2) + 'µWh';
-    this.tvWatchingTime = dto.tvWatchingTime.toFixed(2) + 'μs';
+    this.tvWatchingTime = dto.tvWatchingTime.toFixed(2) + 'ms';
     this.passengerCarMileage = dto.passengerCarMileage.toFixed(2) + 'µm';
     this.subwayTravelDistance = dto.subwayTravelDistance.toFixed(2) + 'mm';
     this.appleProduction = dto.appleProduction.toFixed(2) + 'µg';
