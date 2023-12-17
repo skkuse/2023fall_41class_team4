ddf32e605cb9d3b9a4762bafe420f429b8b8c340
Sori Lim <aint.imsorry@gmail.com>
2023-12-17 14:14:18 +0000

    test: test app controller

--- backend/src/app.controller.spec.ts
+++ backend/src/app.controller.spec.ts
@@ -1,6 +1,11 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DBRepository } from './db/db.repository';
+import { JavaCompilerService } from './java-compiler/java-compiler.service';
+import { JavaRunnerService } from './java-runner/java-runner.service';
+import { ConverterService } from './converter/converter.service';
+import { ConfigService } from '@nestjs/config';
 
 describe('AppController', () => {
   let appController: AppController;
@@ -8,14 +13,25 @@ describe('AppController', () => {
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [
+        AppService,
+        { provide: DBRepository, useValue: {} },
+        { provide: JavaCompilerService, useValue: {} },
+        { provide: JavaRunnerService, useValue: {} },
+        { provide: ConverterService, useValue: {} },
+        {
+          provide: ConfigService,
+          useFactory: () => ({
+            get: () => 1,
+          }),
+        },
+      ],
     }).compile();
 
     appController = app.get<AppController>(AppController);
   });
-  /*
+
   it('should be defined', () => {
     expect(appController).toBeDefined();
   });
-  */
 });
