c60d150bf9ba23afb92e371053aee355ba9a0414
EliseCho <dbwlspdlqj@naver.com>
2023-12-18 00:34:43 +0900

    fix: 파일 이름 변경, 주석 제거 (#87)
    * fix(fe): changed javaeditor to codeEditor
    
    * fix(fe): removed comments in app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,24 +5,19 @@ import Footer from "./components/footer";
 import HowItWorks from "./components/howItWorks";
 import styled from "styled-components";
 import HardwareSpec from "./components/hardwareSpec";
-import JavaEditor from "./components/javaEditor";
+import CodeEditor from "./components/codeEditor";
 import CardComponent from "./components/cardContainer";
 import Status from "./constants/status";
 import axios from "axios";
 
 function App() {
-  //response 여기서 받아서 삼항연산자로 <howitworks/> : <hardwareSpce/><탄소배출량 />로 하여 props로 response 내려보내주기
-
   const defaultValue =
     "// Write down your code here\n// Your class name must be Main";
 
   const editorRef = useRef(null);
 
-  // response state
   const [response, setResponse] = useState(null);
-
   const [status, setStatus] = useState(Status.WAITING);
-
   const [lineCount, setLineCount] = useState();
 
   async function fetchResult(value) {
@@ -33,20 +28,17 @@ function App() {
       setStatus(Status.SUCCESS);
       setResponse(response.data);
     } catch (e) {
-      // compile error or runtime error
       setStatus(Status.COMPILEERROR);
       setResponse(e.response.data);
     }
   }
 
   async function handleSubmit() {
-    // send code to backend
     setStatus(Status.PROGRESS);
     if (lineCount > 1000) {
       setStatus(Status.WAITING);
       alert("1000줄 이내로 작성해주시길 바랍니다.");
     } else {
-      //axios
       await fetchResult(editorRef.current.getValue());
     }
   }
@@ -58,7 +50,6 @@ function App() {
   }
 
   function handleEditorDidMount(editor, _) {
-    // set editorRef.current to editor
     setLineCount(editor.getModel().getLineCount());
     editorRef.current = editor;
   }
@@ -71,7 +62,7 @@ function App() {
     <>
       <Header />
       <Wrapper>
-        <JavaEditor
+        <CodeEditor
           defaultValue={defaultValue}
           editorRef={editorRef}
           status={status}
